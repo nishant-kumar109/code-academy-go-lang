1a8af4f7e5e58ee2a005b0d5c446f1e8732a362a
nishant-gloify <nishantindia18@gmail.com>
2022-03-06 11:11:31 +0530

    var decl and assign

---
+++ zero_value.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Create three variables
+  var emptyInt int8
+  
+  // emptyFloat a float32
+  var emptyFloat  float32
+  // and emptyString a string
+  var emptyString  string
+  // Finally, print them all out
+  fmt.Println(emptyInt, emptyFloat, emptyString)
+}
